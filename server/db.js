93ca8acbeafd2bbcac1da745d87f22231ba438a0
pi2inorevia <mael.tarridec@edu.devinci.fr>
2021-03-09 00:18:24 +0100

    connection and putting data into mongodb atlas

---
+++ server/db.js
@@ -0,0 +1,62 @@
+
+const {MongoClient} = require('mongodb');
+const MONGODB_URI = 'mongodb+srv://louloulepou:louloulepou123@cluster0.zxrqe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const MONGODB_DB_NAME = 'ClearFashion';
+
+async function debut(){
+    const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+    const db = client.db(MONGODB_DB_NAME);
+
+    const dedicatedbrand = require('./sources/dedicatedbrand');
+    async function sandbox () {
+    try {
+        eshop = 'https://www.dedicatedbrand.com/en/men/news';
+        const products = await dedicatedbrand.scrape(eshop);
+        console.log(products);
+        console.log('done');
+        return products
+        process.exit(0);
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+    }
+
+    const products= await sandbox();
+    console.log(products);
+
+
+    const collection = db.collection('products');
+    const result = collection.insertMany(products);
+
+    console.log(result);
+
+    async function query1(){
+        const collection = db.collection('products');
+        const query = await collection.find({'price':{$lt : 79}}); // ici je fais la requete donc await car javascript doit attendre 
+        // de recup la query avant de terminer
+        console.log("$",query);
+    
+    }
+    
+console.log("QUERY1");
+query1();
+}
+
+debut();
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
